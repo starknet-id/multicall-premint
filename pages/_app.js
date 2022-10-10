67d3f531394a2ef0368ac9906dd61595ad023b6c
Thomas Marchand <thomas.marchand@tuta.io>
2022-10-10 23:16:53 +0200

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+import { StarknetConfig, InjectedConnector } from '@starknet-react/core'
 
+function MyApp({ Component, pageProps }) {
+  const connectors = [
+    new InjectedConnector({ options: { id: 'braavos' } }),
+    new InjectedConnector({ options: { id: 'argentX' } }),
+  ]
+  return (
+    <StarknetConfig connectors={connectors}>
+      <Component {...pageProps} />
+    </StarknetConfig>
+  )
+}
 export default MyApp
---
+++ pages/generator.js
@@ -0,0 +1,9 @@
+export function getCalls() {
+
+    return [{
+        contractAddress: "0x049D36570D4e46f48e99674bd3fcc84644DdD6b96F7C741B1562B82f9e004dC7",
+        entrypoint: 'transfer',
+        calldata: ["0x048F24D0D0618fa31813DB91a45d8be6c50749e5E19ec699092CE29aBe809294", 1, 0]
+    }]
+
+}
